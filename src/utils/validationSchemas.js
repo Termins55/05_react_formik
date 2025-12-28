c0bbab0f5a7c508741a552606072f4ffd88282f5
Termins55 <kevgame123@gmail.com>
2025-12-28 17:01:19 +0200

    Add validationSchema

---
+++ src/utils/validationSchemas.js
@@ -0,0 +1,44 @@
+import * as yup from "yup";
+
+export const LOGIN_VALIDATION_SCHEMA = yup.object({
+  email: yup
+    .string()
+    .trim()
+    .email("Please enter a valid email address")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .trim()
+    .min(6, "Password must be at least 6 characters")
+    .max(32, "Password is too long")
+    .required("Password is required"),
+});
+
+export const SIGNUP_VALIDATION_SCHEMA = yup.object({
+  name: yup
+    .string()
+    .trim()
+    .min(2, "Too short!")
+    .max(64, "Too long!")
+    .required("First name is required"),
+  lastName: yup
+    .string()
+    .trim()
+    .min(2, "Too short!")
+    .max(64, "Too long!")
+    .required("Last name is required"),
+  email: yup
+    .string()
+    .email("Please enter a valid email")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .trim()
+    .min(6, "Password must be at least 6 characters")
+    .max(32, "Password is too long")
+    .required("Password is required"),
+  passwordConfirmation: yup
+    .string()
+    .oneOf([yup.ref("password")], "Passwords must match")
+    .required("Please confirm your password"),
+});
