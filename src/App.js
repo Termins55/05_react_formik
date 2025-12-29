160c27e0681a3f3f1bc25b6d62acb7b1511b199b
Termins55 <kevgame123@gmail.com>
2025-12-29 09:05:22 +0200

    Add Snowfall

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Snowfall from "react-snowfall";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
@@ -8,6 +9,7 @@ import BasePage from "./components/BasePage";
 function App() {
   return (
     <Router>
+      <Snowfall color='#82C3D9'/>
       <Routes>
         <Route path="/" element={<BasePage />}>
           <Route index element={<HomePage />} />
